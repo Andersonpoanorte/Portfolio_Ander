900f2a341dd0bd0b100f0029123a2613b1afddd6
Andersonpoanorte <andersonpoanorte1@gmail.com>
2024-09-20 10:42:34 -0300

    Primeiro commit configuração do desenvolvimento

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Test from "./Test";
+import EstiloGlobal from "./styled";
+
+function App() {
+  return (
+    <>
+      <EstiloGlobal />
+      <Test />
+    </>
+  );
+}
+export default App;
